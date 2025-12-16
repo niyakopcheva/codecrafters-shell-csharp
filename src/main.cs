700f08dfd491552d77b9f4c374301abdcd007b91
niyakopcheva <niyakopcheva@gmail.com>
2025-12-16 19:24:09 +0200

    REPL fix

--- src/main.cs
+++ src/main.cs
@@ -3,9 +3,9 @@ class Program
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
         while (true)
         {
+            Console.Write("$ ");
             string command = Console.ReadLine();
             if (command == null || command != null)
                 Console.WriteLine($"{command}: command not found");
