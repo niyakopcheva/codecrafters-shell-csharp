778aab86ccaa948dbfbf0af2dcee75a3ce8adc45
niyakopcheva <niyakopcheva@gmail.com>
2025-12-16 19:22:28 +0200

    implemented REPL

--- src/main.cs
+++ src/main.cs
@@ -4,8 +4,11 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
-        string command = Console.ReadLine();
-        if (command == null || command != null)
-            Console.WriteLine($"{command}: command not found");
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command == null || command != null)
+                Console.WriteLine($"{command}: command not found");
+        }
     }
 }
