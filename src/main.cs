a7e46dfe86ea7a7af6512585840effa43c53d4aa
niyakopcheva <niyakopcheva@gmail.com>
2025-12-18 01:19:14 +0200

    test type builtin

--- src/main.cs
+++ src/main.cs
@@ -1,35 +1,68 @@
-class Program
+using System.Security.Cryptography;
+
+List<string> builtin = new(["exit", "echo", "type"]);
+Dictionary<string, Action<string[]>> commands = new()
 {
-    static void Main()
+    { "echo", echo },
+    { "exit", exit },
+    { "type", type }
+};
+
+while (true)
+{
+    Console.Write("$ ");
+    string command = Console.ReadLine() ?? "";
+    if (command == "") continue;
+
+    string firstCommand = command.Split(" ")[0];
+    string[] arguments = command.Substring(command.IndexOf(' ') + 1).Split(" ");
+    if (commands.ContainsKey(firstCommand))
     {
-        List<string> builtin = new(["exit", "echo", "type"]);
-        while (true)
-        {
-            Console.Write("$ ");
-            string command = Console.ReadLine();
-
-            if (command == "exit") return;
-
-            string firstCommand = command.Split(" ")[0];
-
-            if (firstCommand == "echo")
-            {
-                string[] args = command.Substring(command.IndexOf(' ') + 1).Split(" ");
-                Console.WriteLine(String.Join(" ", args));
-                continue;
-            }
-
-            if (firstCommand == "type")
-            {
-                string arg = command.Substring(command.IndexOf(' ') + 1);
-                if (builtin.Contains(arg))
-                    Console.WriteLine($"{arg} is a shell builtin");
-                else
-                    Console.WriteLine($"{arg}: not found");
-                continue;
-            }
-
-            Console.WriteLine($"{command}: command not found");
-        }
+        commands[firstCommand].Invoke(arguments);
+    }
+    else
+    {
+        System.Console.WriteLine($"{firstCommand}: command not found");
     }
 }
+
+static void echo(string[] arguments)
+{
+    System.Console.WriteLine(String.Join(" ", arguments));
+}
+
+static void exit(string[] arguments)
+{
+    System.Environment.Exit(0);
+}
+
+void type(string[] arguments)
+{
+    string cmd = arguments[0];
+    if (builtin.Contains(cmd))
+        Console.WriteLine($"{cmd} is a shell builtin");
+    else
+    {
+        string? path = Environment.GetEnvironmentVariable("PATH");
+        if (path == null) return;
+
+        string[] directories = path.Split(Path.PathSeparator);
+        foreach (string dir in directories)
+        {
+            if (!Directory.Exists(dir)) continue;
+
+            var files = Directory.GetFiles(dir);
+            var exactFilePath = files.FirstOrDefault(f =>
+            Path.GetFileNameWithoutExtension(f).Equals(cmd, StringComparison.OrdinalIgnoreCase));
+            // var names = files.Select(f => Path.GetFileNameWithoutExtension(f));
+            if (exactFilePath != null)
+            {
+                System.Console.WriteLine($"{cmd} is {exactFilePath}");
+                return;
+            }
+        }
+
+        System.Console.WriteLine($"{cmd}: not found");
+    }
+}
+
